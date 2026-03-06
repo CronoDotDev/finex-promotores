e7f7a3c9f6c582cbaf551ca3f8bdd1ae707627e9
Miguel Angel Alvarado <miguel@maalvarado.com>
2026-03-05 21:13:07 -0500

    refactor: update HubSpot form integration to use the embed script and add dataLayer events for form and conversation activity.

--- src/app/components/contacto/form.component.ts
+++ src/app/components/contacto/form.component.ts
@@ -2,14 +2,22 @@ import { Component, AfterViewInit, ElementRef, ViewChild, Renderer2, inject, Cha
 
 @Component({
   selector: 'app-hubspot-form',
-  template: `<div #hubspotFormContainer id="hubspot-form"></div>`,
+  template: `
+    <div 
+      class="hs-form-frame" 
+      #hubspotFormContainer
+      data-region="na1" 
+      data-form-id="fe58ecf0-8078-480d-922f-a26512ef2a87" 
+      data-portal-id="50394060">
+    </div>
+  `,
   styles: [`
     :host {
       display: block;
       width: 100%;
       background: white;
     }
-    #hubspot-form { 
+    .hs-form-frame { 
       width: 100%; 
     }
   `],
@@ -21,38 +29,52 @@ export class HubspotFormComponent implements AfterViewInit {
 
 
   ngAfterViewInit() {
-    // 1. Explicitly clear the innerHTML
-    this.formContainer.nativeElement.innerHTML = '';
-
-    // 2. Load and Init HubSpot
-    this.loadHubSpotScript().then(() => {
-      this.createForm();
-    });
+    this.loadHubSpotScript();
+    this.initHubSpotEvents();
   }
 
-  private loadHubSpotScript(): Promise<void> {
-    return new Promise((resolve) => {
-      // Check if script is already in the document
-      if (window.hasOwnProperty('hbspt')) {
-        resolve();
-        return;
-      }
+  private initHubSpotEvents() {
+    const w = window as any;
+    w.dataLayer = w.dataLayer || [];
 
-      const script = this.renderer.createElement('script');
-      script.src = 'https://js.hsforms.net/forms/v2.js';
-      script.type = 'text/javascript';
-      script.onload = () => resolve();
-      this.renderer.appendChild(document.head, script);
-    });
-  }
-
-  private createForm() {
-    if ((window as any).hbspt) {
-      (window as any).hbspt.forms.create({
-        portalId: '50394060',
-        formId: 'fe58ecf0-8078-480d-922f-a26512ef2a87',
-        target: '#hubspot-form' // Or use this.formContainer.nativeElement
+    const onConversationsAPIReady = () => {
+      w.HubSpotConversations.on('conversationStarted', () => {
+        w.dataLayer.push({ 'event': 'chat_conversation_started' });
       });
+      w.HubSpotConversations.on('contactAssociated', () => {
+        w.dataLayer.push({ 'event': 'chat_contact_associated' });
+      });
+    };
+
+    w.hsConversationsSettings = {};
+
+    if (w.HubSpotConversations) {
+      onConversationsAPIReady();
+    } else {
+      w.hsConversationsOnReady = [onConversationsAPIReady];
     }
+
+    window.addEventListener('hs-form-event:on-ready', () => {
+      w.dataLayer.push({ 'event': 'form_ready' });
+    });
+
+    window.addEventListener('hs-form-event:on-submission:success', (e: any) => {
+      w.dataLayer.push({ 'form_id': e.detail?.formId, 'event': 'form_submit' });
+    });
+  }
+
+  private loadHubSpotScript() {
+    // Check if script is already in the document
+    const scriptId = 'hubspot-form-script';
+    if (document.getElementById(scriptId)) {
+      return;
+    }
+
+    const script = this.renderer.createElement('script');
+    script.id = scriptId;
+    script.src = 'https://js.hsforms.net/forms/embed/50394060.js';
+    script.type = 'text/javascript';
+    script.defer = true;
+    this.renderer.appendChild(document.head, script);
   }
 }
